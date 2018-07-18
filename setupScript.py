9c77e1ac606839c42ce8bd36209f1aefbe66db7e
bmoyano2706 <40216902+bmoyano2706@users.noreply.github.com>
2018-07-18 16:03:31 -0300

    Add files via upload

---
+++ setupScript.py
@@ -0,0 +1,120 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import json
+import urllib2
+import subprocess
+
+name = ''
+email = ''
+
+
+# Check if Xcode Command Line Tools are installed
+if os.system('xcode-select -p') != 0:
+  print "Installing XCode Tools"
+  os.system('xcode-select --install')
+  print "**************************************************************"
+  print "Install the XCode Command Line Tools and run this script again"
+  print "**************************************************************"
+  exit()
+
+
+# Sudo: Spectacle, ZSH, OSX Settings
+print "\n\nWelcome to the Mac Setup Script by Solstice\n"
+
+# Basic Info
+while name == '':
+  name = raw_input("What's your name and surname?\n").strip()
+
+while email == '' or '@' not in email:
+  email = raw_input("What's your email?\n").strip()
+
+
+def show_notification(text):
+  os.system('osascript -e \'display notification "'+ text +'" with title "Mac Setup"\' > /dev/null')
+
+
+print "Hi %s!" % name
+print "You'll be asked for your password at a few points in the process"
+print "*************************************"
+print "Setting up your Mac..."
+print "*************************************"
+
+# Create a Private Key
+if not os.path.isfile(os.path.expanduser("~") + '/.ssh/id_rsa.pub') :
+  print "Creating your Private Key"
+  os.system('ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N "" -C "%s"' % email)
+
+
+# Set computer name & git info (as done via System Preferences → Sharing)
+os.system('sudo scutil --set ComputerName "%s"' % name)
+os.system('sudo scutil --set HostName "%s"' % name)
+os.system('sudo scutil --set LocalHostName "%s"' % name.replace(' ', '-')) # Doesn't support spaces
+os.system('sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "%s"' % name)
+
+
+# Install Brew & Brew Cask
+print "Installing Brew & Brew Cask"
+os.system('touch ~/.bash_profile')
+os.system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
+os.system('brew tap caskroom/cask')
+os.system('brew tap homebrew/services')
+os.system('brew tap caskroom/versions')
+os.system('brew tap caskroom/fonts')
+os.system('brew update && brew upgrade && brew cleanup && brew cask cleanup')
+
+
+# OSX Tweaks & Essentials
+print "Installing Quicklook Helpers"
+os.system('brew cask install qlcolorcode qlmarkdown quicklook-csv quicklook-json webpquicklook suspicious-package epubquicklook qlstephen qlprettypatch font-hack qlvideo')
+
+
+
+print "Installing Essential Apps"
+os.system('brew cask install spectacle the-unarchiver')
+os.system('brew cask install google-chrome spotify slack zoom adobe-acrobat-reader google-backup-and-sync')
+
+
+#Random OSX Settings
+print "Tweaking OSX Settings"
+
+# Finder: allow text selection in Quick Look
+os.system('defaults write com.apple.finder QLEnableTextSelection -bool true')
+# Check for software updates daily
+os.system('defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1')
+# Disable auto-correct
+#os.system('defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false')
+# Require password immediately after sleep or screen saver begins
+os.system('defaults write com.apple.screensaver askForPassword -int 1')
+os.system('defaults write com.apple.screensaver askForPasswordDelay -int 0')
+# Show the ~/Library folder
+os.system('chflags nohidden ~/Library')
+# Don’t automatically rearrange Spaces based on most recent use
+os.system('defaults write com.apple.dock mru-spaces -bool false')
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+os.system('defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true')
+
+
+# Make Google Chrome the default browser
+os.system('open -a "Google Chrome" --args --make-default-browser')
+
+# Open Spectacle (Needs to be enabled manually)
+os.system('open -a "Spectacle"')
+
+
+# Clean Up
+os.system('brew cleanup && brew cask cleanup')
+
+# Mute startup sound
+show_notification("We need your password")
+os.system('sudo nvram SystemAudioVolume=%00')
+
+
+show_notification("All done! Enjoy your new macOS Thank you for joining Solstice ^^!")
+
+
+
+
+
+
