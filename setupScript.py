8610bdc630bd2c8e2579ce70d43608070a8c4fa4
bmoyano2706 <40216902+bmoyano2706@users.noreply.github.com>
2018-07-19 10:44:08 -0300

    Update setupScript.py
    addesd signin into appstore

--- setupScript.py
+++ setupScript.py
@@ -63,6 +63,9 @@ os.system('brew update && brew upgrade && brew cleanup && brew cask cleanup')
 print "Installing Mas"
 os.system('brew install mas')
 
+#Singin Appstore first time
+os.system('mas signin --dialog "%s"' % email)
+
 #Installing Appstore Apps
 os.system('mas install 409183694')  # Keynote
 os.system('mas upgrade')  # Update all appstore apps
